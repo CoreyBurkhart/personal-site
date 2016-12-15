de1f42f5f3b104456d33516ada101d3e89d2eb5d
Corey Burkhart <hello@coreyburkhart.com>
2016-12-14 18:41:29 -0800

    removed images and adjusted for mobile layout

--- src/components/App.js
+++ src/components/App.js
@@ -13,9 +13,6 @@ import SkillsList from './SkillList.js';
 import SocialIcons from './SocialIcons.js';
 //IMAGES
 import me from '../images/me.jpeg';
-import forest from '../images/forest.jpg';
-import city from '../images/city.jpg';
-import mountains from '../images/mountains.jpg';
 
 class App extends Component {
 
@@ -25,12 +22,12 @@ class App extends Component {
         <ImageBlock id='me' url={me} />
         <Heading />
         <Article title='Need a developer as passionate as you are? Look no further.' text='Whether you love React, want to make a quick marketing site, 	or just need helping hand, I’m your man. I am driven to build fast, reliable and responsive, websites and web applications using the latest standards and best practices. High quality work and great communication are what I offer and aim for 100% satisfaction with every client. TLDR: I turn dreams to css, idea’s into products, and coffee into code. ' />
-        <QuoteBlock quote='"Hands down the best developer I have ever had the pleasure of working with!"' cite='Some Dude' url={mountains} colorSet='quote-color-1' />
+        <QuoteBlock quote='"Hands down the best developer I have ever had the pleasure of working with!"' cite='Some Dude' colorSet='quote-color-1' />
         <SkillsList />
         <LearningList />
-        <QuoteBlock quote='"Hands down the best developer I have ever had the pleasure of working with!"' cite='Some Dude' url={city} colorSet='quote-color-2' />
+        <QuoteBlock quote='"Hands down the best developer I have ever had the pleasure of working with!"' cite='Some Dude' colorSet='quote-color-2' />
         <Article title='Need a developer who lives and breathes standards and best practices?' text='  I understand what it means to work on a team and offer excellent communication to make projects as clear-cut and stress free as possible. I am someone who keeps thinking about the project even after work, and especially when I am working with a good team. I love science and nature.' />
-        <QuoteBlock quote='"Hands down the best developer I have ever had the pleasure of working with!"' cite='Some Dude' url={forest} colorSet='quote-color-1' />
+        <QuoteBlock quote='"Hands down the best developer I have ever had the pleasure of working with!"' cite='Some Dude' colorSet='quote-color-1' />
         <ContactForm />
         <SocialIcons />
         <ContactInfo />
--- src/components/QuoteBlock.jsx
+++ src/components/QuoteBlock.jsx
@@ -6,16 +6,16 @@ import '../css/quote-block.css';
 export default class QuoteBlock extends Component {
 
   render() {
-    let style;
-    if(this.props.url) {
-      style = {
-        backgroundImage: `url(${this.props.url})`,
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: 'cover'
-      }
-    }
+    // let style;
+    // if(this.props.url) {
+    //   style = {
+    //     backgroundImage: `url(${this.props.url})`,
+    //     backgroundRepeat: 'no-repeat',
+    //     backgroundSize: 'cover'
+    //   }
+    // }
     return (
-        <Block fullWidth={true} className={"quote "+ this.props.colorSet} style={style}>
+        <Block fullWidth={true} className={"quote "+ this.props.colorSet} >
           <ScrollEffects animate='fadeIn' >
             <blockquote>{this.props.quote}</blockquote>
             <sub><em>{this.props.cite}</em></sub>
